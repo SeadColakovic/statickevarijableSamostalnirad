0372c7cfd9dbf0578b0700ceb76d8235954c5989
SeadColakovic <flegarsead263@gmail.com>
2025-02-10 13:35:08 +0100

    Add files via upload

---
+++ samostalniRAdstatickevarijable/samostalniRAdstatickevarijable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samostalniRAdstatickevarijable
+{
+ 
+    public class PrvaKlasa
+    {
+        private static int counter;
+
+        public PrvaKlasa()
+        {
+            counter++;
+        }
+
+        public static int Counter
+        {
+            get { return counter; }
+        }
+    }
+
+    PrvaKlasa x = new PrvaKlasa();
+    PrvaKlasa y = new PrvaKlasa();
+    PrvaKlasa z = new PrvaKlasa();
+
+    Console.WriteLine(PrvaKlasa.Counter);
+
+}
